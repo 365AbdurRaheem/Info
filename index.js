578daf3c17d0424bff8aa2ef31d5e06a4f36c18f
Raja Abdur Raheem <130824814+365AbdurRaheem@users.noreply.github.com>
2024-05-26 16:19:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Contacts } = require("@capacitor-community/contacts");
+const { Capacitor } = require('@capacitor/core');
+
+const fetchContacts = async () => {
+    const projection = {
+        name: true,
+        phones: true,
+        PostalAddress: true,
+    };
+
+    if (Capacitor.getPlatform() === 'web') {
+        console.error("Contacts API is not implemented on the web.");
+        return;
+    }
+
+    try {
+        const result = await Contacts.getContacts({ projection });
+        console.log(result);
+    } catch (error) {
+        console.error("Error fetching contacts:", error);
+    }
+};
+
+fetchContacts();
